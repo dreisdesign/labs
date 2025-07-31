a868457cdf63d94cf60223c2aedf7fe4bcf724ef
dreisdesign <danreisdesign@gmail.com>
2025-07-31 14:54:28 -0400

    New "add" icon

---
+++ design-system/components/icons-list.js
@@ -0,0 +1,16 @@
+export default [
+  "add",
+  "add_comment",
+  "bedtime",
+  "bedtime_off",
+  "cancel",
+  "change_circle",
+  "check",
+  "close",
+  "comment",
+  "delete_forever",
+  "edit",
+  "rate_review",
+  "settings",
+  "undo"
+];
