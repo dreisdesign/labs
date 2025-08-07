bd62d178b47fa506042e0bb5913ade2cc2f25bad
dreisdesign <danreisdesign@gmail.com>
2025-08-07 05:20:46 -0400

    labs-icon.js - post deploy

--- design-system/src/components/labs-icon.js
+++ design-system/src/components/labs-icon.js
@@ -5,23 +5,23 @@ const ICON_BASE = window.location.pathname.includes("/labs/")
   ? "/labs/design-system/icons/"
   : "icons/";
 const icons = {
-  add: ICON_BASE + 'add--labs-icons.svg',
-  add_comment: ICON_BASE + 'add_comment--labs-icons.svg',
-  apps: ICON_BASE + 'apps--labs-icons.svg',
-  bedtime: ICON_BASE + 'bedtime--labs-icons.svg',
-  bedtime_off: ICON_BASE + 'bedtime_off--labs-icons.svg',
-  cancel: ICON_BASE + 'cancel--labs-icons.svg',
-  change_circle: ICON_BASE + 'change_circle--labs-icons.svg',
-  check: ICON_BASE + 'check--labs-icons.svg',
-  close: ICON_BASE + 'close--labs-icons.svg',
-  code: ICON_BASE + 'code--labs-icons.svg',
-  comment: ICON_BASE + 'comment--labs-icons.svg',
-  content_copy: ICON_BASE + 'content_copy--labs-icons.svg',
-  delete_forever: ICON_BASE + 'delete_forever--labs-icons.svg',
-  edit: ICON_BASE + 'edit--labs-icons.svg',
-  rate_review: ICON_BASE + 'rate_review--labs-icons.svg',
-  settings: ICON_BASE + 'settings--labs-icons.svg',
-  undo: ICON_BASE + 'undo--labs-icons.svg'
+    add: ICON_BASE + 'add--labs-icons.svg',
+    add_comment: ICON_BASE + 'add_comment--labs-icons.svg',
+    apps: ICON_BASE + 'apps--labs-icons.svg',
+    bedtime: ICON_BASE + 'bedtime--labs-icons.svg',
+    bedtime_off: ICON_BASE + 'bedtime_off--labs-icons.svg',
+    cancel: ICON_BASE + 'cancel--labs-icons.svg',
+    change_circle: ICON_BASE + 'change_circle--labs-icons.svg',
+    check: ICON_BASE + 'check--labs-icons.svg',
+    close: ICON_BASE + 'close--labs-icons.svg',
+    code: ICON_BASE + 'code--labs-icons.svg',
+    comment: ICON_BASE + 'comment--labs-icons.svg',
+    content_copy: ICON_BASE + 'content_copy--labs-icons.svg',
+    delete_forever: ICON_BASE + 'delete_forever--labs-icons.svg',
+    edit: ICON_BASE + 'edit--labs-icons.svg',
+    rate_review: ICON_BASE + 'rate_review--labs-icons.svg',
+    settings: ICON_BASE + 'settings--labs-icons.svg',
+    undo: ICON_BASE + 'undo--labs-icons.svg'
 };
 
 class LabsIcon extends HTMLElement {
