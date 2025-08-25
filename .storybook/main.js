8e0b699d698d2a547412dbb835f2f0aac68c3470
dreisdesign <danreisdesign@gmail.com>
2025-08-25 07:03:58 -0400

    chore(storybook): confirm main.js config for 9.1.3

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,17 +3,16 @@ module.exports = {
     '../design-system/src/**/*.stories.@(js|jsx|ts|tsx|mdx)',
     '../src/**/*.stories.@(js|jsx|ts|tsx|mdx)'
   ],
+
   addons: [
     '@storybook/addon-docs',
     '@storybook/addon-a11y',
     '@storybook/addon-themes',
     '@storybook/addon-vitest'
   ],
+
   framework: {
     name: '@storybook/web-components-vite',
     options: {}
-  },
-  docs: {
-    autodocs: 'tag',
-  },
+  }
 };
