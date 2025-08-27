cd84ded3d150efbcdf019df9a4051714b104fa6d
dreisdesign <danreisdesign@gmail.com>
2025-08-27 12:10:08 -0400

    Timestamp auto injection test

--- scripts/update-labs-timestamp.sh
+++ scripts/update-labs-timestamp.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-# Update Labs index.html timestamp placeholder with latest git commit date
+# Update Labs index.html timestamp with latest git commit date
 STAMP=$(git log -1 --format='%cd' --date='format:%B %d, %Y, %I:%M %p %Z')
-sed -i '' "s|<!--GIT_TIMESTAMP-->|$STAMP|g" index.html
+# Replace the Last updated line with the new timestamp
+sed -i '' "s|Last updated:.*|Last updated: $STAMP|g" index.html
