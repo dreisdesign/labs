675a091cf57727a9710576fd8aa4022461332cd0
Dan Reis Design <danreisdesign@gmail.com>
2025-10-08 05:51:55 -0400

    refactor: move Add and Reset All Data button stories from Patterns to Wrapped Components for correct modular classification

---
+++ design-system/src/stories/components-wrapped/labs-button.wrapped.stories.js
@@ -0,0 +1,27 @@
+import '../../components/labs-button.js';
+import '../../components/labs-icon.js';
+
+export default {
+  title: '3. Components (Wrapped)/Button',
+  component: 'labs-button',
+};
+
+export const Add = {
+  name: 'Add',
+  render: () => `
+    <labs-button pill variant="primary">
+      <labs-icon slot="icon-left" name="add"></labs-icon>
+      Add
+    </labs-button>
+  `,
+};
+
+export const ResetAllData = {
+  name: 'Reset all data',
+  render: () => `
+    <labs-button pill variant="destructive">
+      <labs-icon slot="icon-left" name="delete_forever"></labs-icon>
+      Reset All Data
+    </labs-button>
+  `,
+};
